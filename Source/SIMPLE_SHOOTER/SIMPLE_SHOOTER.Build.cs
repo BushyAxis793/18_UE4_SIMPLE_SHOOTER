9cf330ad9b2fa9b8fa4d68452aaf2319c139a50b
Mikołaj Nawrocki <m.nawrocki7932gmail.com>
2020-12-06 13:16:47 +0100

    200. Custom BTTasks In C++

--- Source/SIMPLE_SHOOTER/SIMPLE_SHOOTER.Build.cs
+++ Source/SIMPLE_SHOOTER/SIMPLE_SHOOTER.Build.cs
@@ -8,7 +8,7 @@ public class SIMPLE_SHOOTER : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore","GameplayTasks" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
